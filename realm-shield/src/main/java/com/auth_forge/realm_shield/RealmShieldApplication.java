63ef692bf0abfdb0accac4ba2e8ebac5a52b9c31
Moad <elfatihi.moad@gmail.com>
2025-05-01 18:04:35 +0200

    setup project

---
+++ realm-shield/src/main/java/com/auth_forge/realm_shield/RealmShieldApplication.java
@@ -0,0 +1,13 @@
+package com.auth_forge.realm_shield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealmShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealmShieldApplication.class, args);
+	}
+
+}
---
+++ realm-shield/src/test/java/com/auth_forge/realm_shield/RealmShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth_forge.realm_shield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealmShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
