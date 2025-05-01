972aee4505e87f5f0bd0b5bf391688ccca1a7186
Moad <elfatihi.moad@gmail.com>
2025-05-02 00:47:09 +0200

    add product microservice

---
+++ products/src/main/java/com/products/controller/Products.java
@@ -0,0 +1,20 @@
+package com.products.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class Products {
+
+	@GetMapping("/list")
+	public String getAllProducts(){
+		return "products";
+	}
+
+	@GetMapping("/anys")
+	public String getAllProduct(){
+		return "products";
+	}
+}
